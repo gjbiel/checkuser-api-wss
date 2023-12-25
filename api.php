e1992934967df3e455d44fe4007e899c02aa25cf
Gabriel <82896735+gjbiel@users.noreply.github.com>
2023-12-25 00:55:30 -0300

    Update api.php

--- api.php
+++ api.php
@@ -17,36 +17,61 @@ function ip_autorizado($clientIp, $allowedIps) {
 // Função principal que processa a requisição
 function processar_requisicao() {
     $requestMethod = $_SERVER['REQUEST_METHOD'];
-    $url = isset($_GET['url']) ? $_GET['url'] : '';
-    $urlParts = parse_url($url);
-    $clientIp = isset($urlParts['host']) ? $urlParts['host'] : '';
+    $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $urlParts = explode('/API.PHP/', $urlPath);
+
+    // A parte da URL após /API.PHP/
+    $url = isset($urlParts[1]) ? $urlParts[1] : '';
+
+    var_dump($urlPath);  // Adicionado para depurar
+    var_dump($urlParts); // Adicionado para depurar
+
+    if (empty($url)) {
+        http_response_code(400);
+        die("URL não especificada.");
+    }
+
+    $clientIp = $_SERVER['REMOTE_ADDR'];
 
     $allowedIps = ler_arquivo_json();
 
-    if ($requestMethod === 'GET' || $requestMethod === 'POST') {
+    if (!ip_autorizado($clientIp, $allowedIps)) {
+        http_response_code(403);
+        die("Acesso não autorizado.");
+    }
 
+    try {
         if ($requestMethod === 'POST') {
             // Ler dados POST diretamente da solicitação
             $postData = file_get_contents('php://input');
-            if ($postData !== false) {
-                echo file_get_contents($url, false, stream_context_create([
-                    'http' => [
-                        'method' => 'POST',
-                        'header' => 'Content-Type: application/json',
-                        'content' => $postData,
-                    ],
-                ]));
-            } else {
-                echo "Erro ao ler dados POST.";
+
+            if ($postData === false) {
+                throw new Exception("Erro ao ler dados POST.");
             }
+
+            $options = [
+                'http' => [
+                    'method' => 'POST',
+                    'header' => 'Content-Type: application/json',
+                    'content' => $postData,
+                ],
+            ];
+
+            $context = stream_context_create($options);
+            $response = file_get_contents($url, false, $context);
+
+            if ($response === false) {
+                throw new Exception("Erro ao obter resposta da URL.");
+            }
+
+            echo $response;
         } elseif ($requestMethod === 'GET') {
-            echo file_get_contents($url);
-        }
+            $response = file_get_contents($url);
 
-    } else {
-        echo "Método não suportado.";
-    }
-}
+            if ($response === false) {
+                throw new Exception("Erro ao obter resposta da URL.");
+            }
 
-processar_requisicao();
-?>
+            echo $response;
+        } else {
+            http_response_code(40
